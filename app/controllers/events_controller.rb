d9e0df899054b301a2e9336bb88eb7d33b13b117
Sebastian Beraud <beraudsebastian@gmail.com>
2020-11-25 14:42:12 +0100

    setup routes and controller + refactoring

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,9 +1,20 @@
 class EventsController < ApplicationController
+  before_action :set_event, only: [ :show, :destroy ]
   def index
     @events = Event.all
   end
 
   def show
+  end
+
+  def destroy
+    @event = Event.destroy
+    redirect_to events_path
+  end
+
+  private
+
+  def set_event
     @event = Event.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   
-  resources :events, only: [ :index ]
+  resources :events, only: [ :index, :destroy ]
 end
