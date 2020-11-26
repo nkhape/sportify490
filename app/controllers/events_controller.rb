e5cc3513bef7a39634526dad186d137b6b2dab34
Ioan-Andrei Ojica <ivan.andrei93@mail.ru>
2020-11-26 13:04:32 +0100

    the view finally appears, now it just needs to be edited

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,10 +1,11 @@
 class EventsController < ApplicationController
+before_action :set_event, only: [:edit, :update, :show]
+
   def index
     @events = Event.all
   end
 
   def show
-    @event = Event.find(params[:id])
   end
 
   def new
@@ -22,9 +23,12 @@ class EventsController < ApplicationController
     end
     # need to add  'authorize @listing' when we have pundit
 
+    def edit
+    end
+
     def update
       if @event.update(event_params)
-        redirect_to event_path(@event)
+        redirect_to events_path
       else
         render :edit
       end
@@ -32,6 +36,10 @@ class EventsController < ApplicationController
   end
 
   private
+
+  def set_event
+    @event = Event.find(params[:id])
+  end
 
   def event_params
     params.require(:event).permit(:name, :description, :location, :date, :price, :capacity, :level, :sport)
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  resources :events, only: [ :index, :new, :create, :edit ]
+  resources :events, only: [ :index, :new, :create, :edit, :update ]
 end
