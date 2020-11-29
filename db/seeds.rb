3a3bcd6ce8e37b0e29a43af4c76d381cfa7e7ca3
Camille Rey <camille.celine.rey@gmail.com>
2020-11-29 19:09:25 +0100

    change booking logic and add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -32,6 +32,73 @@ andrea = User.new(
 andrea.photo.attach(io: File.open("#{Rails.root}/db/images/andrea.jpg"), filename: "andrea.jpg")
 andrea.save!
 
+adam = User.new(
+ username: "adam",
+ location: "Kotbusser Tor Berlin",
+ email: "adam@hello.com",
+ password: "123456",
+ description: "Advanced soccer player. Wants to practice regularly."
+)
+
+adam.photo.attach(io: File.open("#{Rails.root}/db/images/adam.jpeg"), filename: "adam.jpeg")
+adam.save!
+
+elisa = User.new(
+ username: "elisa",
+ location: "Kotbusser Tor Berlin",
+ email: "elisa@hello.com",
+ password: "123456",
+ description: "Advanced soccer player. Wants to practice regularly."
+)
+
+elisa.photo.attach(io: File.open("#{Rails.root}/db/images/elisa.jpg"), filename: "elisa.jpg")
+elisa.save!
+
+kelly = User.new(
+ username: "kelly",
+ location: "Kotbusser Tor Berlin",
+ email: "kelly@hello.com",
+ password: "123456",
+ description: "Advanced soccer player. Wants to practice regularly."
+)
+
+kelly.photo.attach(io: File.open("#{Rails.root}/db/images/kelly.jpeg"), filename: "kelly.jpeg")
+kelly.save!
+
+marvin = User.new(
+ username: "marvin",
+ location: "Kotbusser Tor Berlin",
+ email: "marvin@hello.com",
+ password: "123456",
+ description: "Advanced soccer player. Wants to practice regularly."
+)
+
+marvin.photo.attach(io: File.open("#{Rails.root}/db/images/marvin.jpeg"), filename: "marvin.jpeg")
+marvin.save!
+
+simone = User.new(
+ username: "simone",
+ location: "Kotbusser Tor Berlin",
+ email: "simone@hello.com",
+ password: "123456",
+ description: "Advanced soccer player. Wants to practice regularly."
+)
+
+simone.photo.attach(io: File.open("#{Rails.root}/db/images/simone.jpg"), filename: "simone.jpg")
+simone.save!
+
+sven = User.new(
+ username: "sven",
+ location: "Kotbusser Tor Berlin",
+ email: "sven@hello.com",
+ password: "123456",
+ description: "Advanced soccer player. Wants to practice regularly."
+)
+
+sven.photo.attach(io: File.open("#{Rails.root}/db/images/sven.jpeg"), filename: "sven.jpeg")
+sven.save!
+
+
 event1 = Event.create(
  name: "Tennis match 1 on 1 bring your racquet",
  description: "Tennis practice for beginner.",
