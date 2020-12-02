6d8a5f2b1936a3a2d3f1212d8eb4c21c5694001c
Camille Rey <camille.celine.rey@gmail.com>
2020-12-02 12:28:34 +0100

    More booking logiic and log out link

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,6 @@
 class Event < ApplicationRecord
   belongs_to :user
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
   has_many :users, through: :bookings
 
   include PgSearch::Model
