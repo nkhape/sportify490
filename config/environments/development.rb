7787589c12d3b71b4f5c22485ed8fdba1aa0dec0
Camille Rey <camille.celine.rey@gmail.com>
2021-02-24 15:42:07 +0100

    add container to sign up form

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,5 +1,7 @@
 Rails.application.configure do
   config.action_mailer.default_url_options = { host: "http://localhost:3000" }
+  # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  # change host in production !
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
